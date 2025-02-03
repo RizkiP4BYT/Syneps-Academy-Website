e0597f0d7644492f3fbd1f291bd258a15fcc6fe3
RizkiP4BYT <47355568+RizkiP4BYT@users.noreply.github.com>
2025-02-03 09:48:19 +0000

    Fixing bad layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,58 +1,56 @@
-'use client'
-import { Box, Container, Drawer, styled } from '@mui/material'
-import React, { useState } from 'react'
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
+"use client";
+import {
+  Box,
+  Container,
+  Drawer,
+  IconButton,
+  styled,
+  useMediaQuery,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React, { useEffect, useState } from "react";
+import Header from "./layout/Header";
+import { Sidebar } from "react-pro-sidebar";
+import SidebarMenu from "./layout/SidebarMenu";
 
-const MainWrapper = styled('div')(() => ({
-    display: 'flex',
-    minHeight: '100vh',
-    width: '100%'
-}))
+const MainWrapper = styled("div")(() => ({
+  display: "flex",
+  minHeight: "100vh",
+  width: "100%",
+}));
 
-const PageWrapper = styled('div')(() => ({
-    display: 'flex',
-    flexGrow: 1,
-    paddingBottom: '60px',
-    flexDirection: 'column',
-    zIndex: 1,
-    backgroundColor: 'transparent'
-}))
-
-const Header = styled('div')(({ theme }) => ({
-    boxShadow: 'none',
-    background: theme.palette.background.paper,
-    justifyContent: 'center',
-    backdropFilter: 'blur(4px)',
-    [theme.breakpoints.up('lg')]: {
-        minHeight: '70px'
-    }
-}))
+const PageWrapper = styled("div")(() => ({
+  display: "flex",
+  flexGrow: 1,
+  paddingBottom: "60px",
+  flexDirection: "column",
+  zIndex: 1,
+  backgroundColor: "transparent",
+}));
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const [sidebarOpen, isSidebarOpen] = useState(false)
-    return (
-        <MainWrapper>
-            <Sidebar toggled={sidebarOpen} breakPoint="lg">
-                <Menu>
-                    <SubMenu label="Charts">
-                        <MenuItem> Pie charts </MenuItem>
-                        <MenuItem> Line charts </MenuItem>
-                    </SubMenu>
-                    <MenuItem> Documentation </MenuItem>
-                    <MenuItem> Calendar </MenuItem>
-                </Menu>
-            </Sidebar>
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  return (
+    <MainWrapper>
+      {mounted && (
+        <Sidebar
+          toggled={sidebarOpen}
+          onBackdropClick={() => setSidebarOpen(false)}
+          breakPoint="lg"
+          backgroundColor="#e3e3e3"
+        >
+          <SidebarMenu />
+        </Sidebar>
+      )}
+      <PageWrapper>
+        <Header toggleMobileSidebar={() => setSidebarOpen(true)} />
 
-            <Header>
-                <Drawer />
-                <p>WOwowowowo</p>
-            </Header>
-
-            <PageWrapper>
-                <Container sx={{ paddingTop: '20px', maxWidth: '1200px' }}>
-                    <Box sx={{ minHeight: 'calc(100vh - 170px)' }}>{children}</Box>
-                </Container>
-            </PageWrapper>
-        </MainWrapper>
-    )
+        <Container sx={{ paddingTop: "20px", maxWidth: "1200px" }}>
+          <Box sx={{ minHeight: "calc(100vh - 170px)" }}>{children}</Box>
+        </Container>
+      </PageWrapper>
+    </MainWrapper>
+  );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,9 @@
-import { Box, Grid2 } from '@mui/material'
+import { Box, Grid2 } from "@mui/material";
 
 export default function HomeDashboard() {
-    return (
-        <Box>
-            <Grid2 container spacing={3}>
-                <Grid2 marginX={12}>
-                    <h1>Hello, world! I'm ready to be edited ^-^</h1>
-                </Grid2>
-            </Grid2>
-        </Box>
-    )
+  return (
+    <Box>
+      <h1>Hello, world! I'm ready to be edited ^-^</h1>
+    </Box>
+  );
 }
