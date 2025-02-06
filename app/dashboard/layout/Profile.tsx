29ce814e5851ef17bd6b19b49d111d1b6b52fc66
RizkiP4BYT <47355568+RizkiP4BYT@users.noreply.github.com>
2025-02-06 03:56:08 +0000

    Fixing profile component

--- app/dashboard/layout/Profile.tsx
+++ app/dashboard/layout/Profile.tsx
@@ -1,21 +1,22 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { Box, Menu, MenuItem, Switch, Typography, styled } from "@mui/material";
-import Image from "next/image";
+'use client'
+import React, { Suspense, useEffect, useState } from 'react'
+import { Box, Menu, MenuItem, Switch, Typography, styled } from '@mui/material'
+import Image from 'next/image'
 // import Link from 'next/link'
 // import { Avatar, Box, Menu, Button, IconButton, MenuItem, ListItemIcon, ListItemText, Typography, styled } from '@mui/material'
 // // import fetchUser from '@/lib/fetchUser'
 // import { createClient } from '@/utils/supabase/server'
 // import ProfileMenu from '../../components/shared/ProfileMenu'
 // import ProfileImage from '../../components/shared/ProfileImage'
-// import fetchUser from '@/lib/fetchUser'
-// import { User } from '@supabase/supabase-js'
+import fetchUser from '@/lib/fetchUser'
+import { User } from '@supabase/supabase-js'
+import { signout } from '@/lib/auth-actions'
 
-// interface fetchUserType = {
-//     data: {
-//         user: User
-//     }
-// }
+interface fetchUserType {
+    data: {
+        user: User
+    }
+}
 // const Data = ({ dataPromise ) => {
 //     const { data } = use(dataPromise)
 //     if (data.user) {
@@ -48,60 +49,75 @@ import Image from "next/image";
 
 // export default Profile
 const ProfileButtonContainer = styled(Box)({
-  display: "flex",
-  alignItems: "center",
-  border: "1px solid #ccc",
-  borderRadius: "50px",
-  padding: "5px 10px",
-  cursor: "pointer",
-});
+    display: 'flex',
+    alignItems: 'center',
+    border: '1px solid #ccc',
+    borderRadius: '50px',
+    padding: '5px 10px',
+    cursor: 'pointer'
+})
 
 const ProfileMenu = ({ children }: { children: React.ReactNode }) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [darkMode, setDarkMode] = useState<boolean>(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const [darkMode, setDarkMode] = useState<boolean>(false)
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
 
-  const toggleDarkMode = () => {
-    setDarkMode((prev) => !prev);
-  };
+    const toggleDarkMode = () => {
+        setDarkMode((prev) => !prev)
+    }
 
-  return (
-    <div>
-      <ProfileButtonContainer onClick={handleClick}>
-        {children}
-      </ProfileButtonContainer>
+    return (
+        <div>
+            <ProfileButtonContainer onClick={handleClick}>{children}</ProfileButtonContainer>
 
-      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-        <MenuItem onClick={handleClose}>Settings</MenuItem>
-        <MenuItem>
-          <Typography variant="body1">Dark Mode</Typography>
-          <Switch checked={darkMode} onChange={toggleDarkMode} />
-        </MenuItem>
-        <MenuItem onClick={handleClose}>Keluar</MenuItem>
-      </Menu>
-    </div>
-  );
-};
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+                <MenuItem onClick={handleClose}>Settings</MenuItem>
+                <MenuItem>
+                    <Typography variant="body1">Dark Mode</Typography>
+                    <Switch checked={darkMode} onChange={toggleDarkMode} />
+                </MenuItem>
+                <MenuItem onClick={() => signout()}>Keluar</MenuItem>
+            </Menu>
+        </div>
+    )
+}
+
+const UserData = () => {
+    const [user, setUser] = useState<User | null>(null)
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await fetchUser()
+            setUser(data?.user)
+        }
+
+        fetchData()
+    }, [])
+
+    if (user) {
+        return (
+            <ProfileMenu>
+                <Image src={user.user_metadata.picture ?? 'https://docs.gravatar.com/wp-content/uploads/2024/09/cropped-72x72-1.png?w=240'} alt="User Profile" width={40} height={40} />
+                {user.email}
+            </ProfileMenu>
+        )
+    } else {
+        return <p>Login!</p>
+    }
+}
 
 const Profile = () => {
-  return (
-    <ProfileMenu>
-      <Image
-        src="https://docs.gravatar.com/wp-content/uploads/2024/09/cropped-72x72-1.png?w=240"
-        alt="User Profile"
-        width={40}
-        height={40}
-      />
-      Ahmad Cihuy
-    </ProfileMenu>
-  );
-};
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <UserData />
+        </Suspense>
+    )
+}
 
-export default Profile;
+export default Profile
