ed7869e09c6717e6eb31d9f53a9b43a3d655a8bb
RizkiP4BYT <47355568+RizkiP4BYT@users.noreply.github.com>
2025-02-10 06:09:34 +0000

    Setting CustomSnackbar to Right-Bottom Position

--- app/components/CustomSnackbar.tsx
+++ app/components/CustomSnackbar.tsx
@@ -1,40 +1,30 @@
-import React from "react";
-import { Snackbar, Alert, Slide } from "@mui/material";
+import React from 'react'
+import { Snackbar, Alert, Slide } from '@mui/material'
 
 interface CustomSnackbarProps {
-  open: boolean;
-  onClose: () => void;
-  message: string;
-  severity: "success" | "error" | "info" | "warning";
+    open: boolean
+    onClose: () => void
+    message: string
+    severity: 'success' | 'error' | 'info' | 'warning'
 }
 
-const CustomSnackbar: React.FC<CustomSnackbarProps> = ({
-  open,
-  onClose,
-  message,
-  severity,
-}) => {
-  return (
-    <Snackbar
-      open={open}
-      onClose={onClose}
-      autoHideDuration={5000}
-      TransitionComponent={Slide}
-    >
-      <Alert
-        severity={severity}
-        sx={{
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          padding: "16px",
-          fontSize: "1rem",
-        }}
-      >
-        {message}
-      </Alert>
-    </Snackbar>
-  );
-};
+const CustomSnackbar: React.FC<CustomSnackbarProps> = ({ open, onClose, message, severity }) => {
+    return (
+        <Snackbar open={open} onClose={onClose} autoHideDuration={5000} TransitionComponent={Slide} anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
+            <Alert
+                severity={severity}
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    padding: '16px',
+                    fontSize: '1rem'
+                }}
+            >
+                {message}
+            </Alert>
+        </Snackbar>
+    )
+}
 
-export default CustomSnackbar;
+export default CustomSnackbar
