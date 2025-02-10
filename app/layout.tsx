759ec9d7a90aaeb68835ffaaabca011da103258e
RizkiP4BYT <47355568+RizkiP4BYT@users.noreply.github.com>
2025-02-10 06:07:56 +0000

    Adding React-Query Package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,22 @@
-"use client";
-import type { Metadata } from "next";
-import dynamic from "next/dynamic";
-const ThemeProvider = dynamic(
-  () => import("@mui/material").then((e) => e.ThemeProvider),
-  { ssr: false }
-);
-const CssBaseline = dynamic(
-  () => import("@mui/material").then((e) => e.CssBaseline),
-  { ssr: false }
-);
-import { mainTheme } from "@/utils/theme";
+'use client'
+import type { Metadata } from 'next'
+import dynamic from 'next/dynamic'
+const ThemeProvider = dynamic(() => import('@mui/material').then((e) => e.ThemeProvider), { ssr: false })
+const CssBaseline = dynamic(() => import('@mui/material').then((e) => e.CssBaseline), { ssr: false })
+import { mainTheme } from '@/utils/theme'
+import { ReactQueryClientProvider } from './components/ReactQueryClientProvider'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider theme={mainTheme}>
-          <CssBaseline />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <ReactQueryClientProvider>
+            <html lang="en" suppressHydrationWarning>
+                <body>
+                    <ThemeProvider theme={mainTheme}>
+                        <CssBaseline />
+                        {children}
+                    </ThemeProvider>
+                </body>
+            </html>
+        </ReactQueryClientProvider>
+    )
 }
