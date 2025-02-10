66cca1b6d378ee1ac9667a74e11ee730c59530aa
RizkiP4BYT <47355568+RizkiP4BYT@users.noreply.github.com>
2025-02-10 08:14:20 +0000

    Adding log when comes error and Adding JSON validator

--- app/api/program/route.ts
+++ app/api/program/route.ts
@@ -11,9 +11,9 @@ export async function GET() {
       .order("id", { ascending: true });
 
     if (error) throw error;
-    console.log(data)
     return NextResponse.json(data);
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { error: "Gagal mengambil data program" },
       { status: 500 }
@@ -25,6 +25,16 @@ export async function POST(request: Request) {
   const supabase = await createClient();
   const body = await request.json();
 
+  if (!body.nama_program || !body.deskripsi_program) {
+    return NextResponse.json(
+      {
+        error:
+          "Diperlukan 'nama_program', dan 'deskripsi_program' untuk melanjutkan.",
+      },
+      { status: 400 }
+    );
+  }
+
   try {
     const { data, error } = await supabase
       .from("Program")
@@ -34,7 +44,7 @@ export async function POST(request: Request) {
     if (error) throw error;
     return NextResponse.json(data[0]);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return NextResponse.json(
       { error: "Gagal membuat program" },
       { status: 500 }
@@ -46,6 +56,16 @@ export async function PUT(request: Request) {
   const supabase = await createClient();
   const body = await request.json();
 
+  if (!body.id || !body.nama_program || !body.deskripsi_program) {
+    return NextResponse.json(
+      {
+        error:
+          "Diperlukan 'id', 'nama_program', dan 'deskripsi_program' untuk melanjutkan.",
+      },
+      { status: 400 }
+    );
+  }
+
   try {
     const { data, error } = await supabase
       .from("Program")
@@ -56,6 +76,7 @@ export async function PUT(request: Request) {
     if (error) throw error;
     return NextResponse.json(data[0]);
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { error: "Gagal memperbarui program" },
       { status: 500 }
@@ -67,15 +88,19 @@ export async function DELETE(request: Request) {
   const supabase = await createClient();
   const { id } = await request.json();
 
+  if (!id) {
+    return NextResponse.json(
+      { error: "Kamu harus memasukkan 'id' untuk penghapusan" },
+      { status: 400 }
+    );
+  }
   try {
-    const { error } = await supabase
-      .from("Program")
-      .delete()
-      .eq("id", id);
+    const { error } = await supabase.from("Program").delete().eq("id", id);
 
     if (error) throw error;
     return NextResponse.json({ success: true });
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { error: "Gagal menghapus program" },
       { status: 500 }
