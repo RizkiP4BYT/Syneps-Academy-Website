b7c6cebe0717d6d8a96024689a6b38636cbfd694
RizkiP4BYT <rizkip4bytbest@gmail.com>
2025-02-20 14:06:02 +0700

    Adding LocalizationProvider on Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,23 @@ const ThemeProvider = dynamic(() => import('@mui/material').then((e) => e.ThemeP
 const CssBaseline = dynamic(() => import('@mui/material').then((e) => e.CssBaseline), { ssr: false })
 import { mainTheme } from '@/utils/theme'
 import { ReactQueryClientProvider } from './components/ReactQueryClientProvider'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { id } from 'date-fns/locale'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <ReactQueryClientProvider>
-            <html lang="en" suppressHydrationWarning>
-                <body>
-                    <ThemeProvider theme={mainTheme}>
-                        <CssBaseline />
-                        {children}
-                    </ThemeProvider>
-                </body>
-            </html>
+            <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={id}>
+                <html lang="en" suppressHydrationWarning>
+                    <body>
+                        <ThemeProvider theme={mainTheme}>
+                            <CssBaseline />
+                            {children}
+                        </ThemeProvider>
+                    </body>
+                </html>
+            </LocalizationProvider>
         </ReactQueryClientProvider>
     )
 }
